f13cb5db7675e71315bc52ddabeb90865321d08f
jc3213 <jc3213@users.noreply.github.com>
2016-02-04 21:28:53 +0800

    Create storage.js

---
+++ soWatch-mk2/lib/storage.js
@@ -0,0 +1,10 @@
+"use strict";
+
+exports.option = new Object();
+exports.menuitem = new Array();
+exports.website = new Object();
+exports.player = new Object();
+exports.filter = new Object();
+exports.file = new Object();
+exports.when = parseInt(Date.now() / 1000);
+
