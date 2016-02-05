91232650d0f890f601bd7829798eca1b96f1ca3b
jc3213 <jc3213@users.noreply.github.com>
2016-02-05 14:11:20 +0800

    Update storage.js

--- soWatch-mk2/lib/storage.js
+++ soWatch-mk2/lib/storage.js
@@ -2,9 +2,9 @@
 
 exports.option = new Object();
 exports.menuitem = new Array();
+exports.button = new Array();
 exports.website = new Object();
 exports.player = new Object();
 exports.filter = new Object();
 exports.file = new Object();
 exports.when = parseInt(Date.now() / 1000);
-
