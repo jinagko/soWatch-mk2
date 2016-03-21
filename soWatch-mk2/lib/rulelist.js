8520aa4b862660d155acc7482aca4ea0837ffba8
jc3213 <jc3213@users.noreply.github.com>
2016-03-22 07:45:26 +0800

    Update rulelist.js

--- soWatch-mk2/lib/rulelist.js
+++ soWatch-mk2/lib/rulelist.js
@@ -37,7 +37,7 @@ exports.website = [
     0,
     "http://www.le.com/",
     [true, [["letv_player", "letv.swf", true, null, ["http://player.letvcdn.com/*/*/*/*/*/*/newplayer/LetvPlayer.swf*", "http://player.letvcdn.com/*/*/*/*/newplayer/SDKLetvPlayer.swf*"]], ["letv_pccs", "http://www.le.com/cmsdata/playerapi/pccs_sdk_20141113.xml", false, null, ["http://www.le.com/cmsdata/playerapi/pccs_PlayerSDK*.xml*", "http://www.le.com/cmsdata/playerapi/pccs_LiveSDK*.xml*", "http://www.le.com/cmsdata/playerapi/pccs_main*.xml"]]]],
-    [true, [["letv_filter", null, null, false, ["http://*/*/*/*/letv-gug/*/ver_*-avc-*-aac-*.letv*", "http://*.letvimg.com/lc*_gugwl/*/*/*/*/*", "http://*.letvimg.com/lc*_diany/*/*/*/*/*"]]]]
+    [true, [["letv_filter", null, null, false, ["http://*/*/*/*/letv-gug/*/ver_*-avc-*-aac-*.mp4*", "http://*.letvimg.com/lc*_gugwl/*/*/*/*/*", "http://*.letvimg.com/lc*_diany/*/*/*/*/*"]]]]
   ],
   [
     "sohu",
